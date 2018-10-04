5fca690250d5a8021ba94905884af4bde1d7c6e7
RJ Ewing <ewing.rj@protonmail.com>
2018-10-04 11:37:35 -0700

    added deploy script

---
+++ scripts/deployProduction.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+ssh deploy@biscicol3.acis.ufl.edu <<'ENDSSH'
+
+    cd code/prod/bcid
+
+    git fetch
+    git checkout master
+    git pull
+
+    ./gradlew clean &&
+    JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 ./gradlew -PforceJars=true -Penvironment=production fatWar &&
+    sudo cp /home/deploy/code/prod/biscicol-fims/dist/bcid-fat.war /opt/web/prod/webapps/bcid.war &&
+    sudo /bin/touch /opt/web/prod/webapps/bcid.xml
+
+ENDSSH
